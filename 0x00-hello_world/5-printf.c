3109c18de96ed0a33b1cc88df5d50fe3f6f30cf2
donna <donnagaya@gmail.com>
2023-04-20 13:47:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints string followed by a new line
+ *
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
